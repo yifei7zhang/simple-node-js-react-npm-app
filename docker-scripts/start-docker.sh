b3961cd467adfb3c0159d55e9ddbd6636890f439
Yifei Zhang <yifei7.zhang@gmail.com>
2020-01-06 21:15:15 -0500

    add docker scripts

---
+++ docker-scripts/start-docker.sh
@@ -0,0 +1,8 @@
+docker container run --name jenkins-docker --rm --detach \
+  --privileged --network jenkins --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume "$HOME":/home \
+  docker:dind
+
---
+++ docker-scripts/start.sh
@@ -0,0 +1,7 @@
+docker container run --name jenkins-tutorial --rm --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  --volume "$HOME":/home --publish 8080:8080 jenkinsci/blueocean
+
